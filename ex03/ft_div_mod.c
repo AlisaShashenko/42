3ef0ff895d2e992813f8176571807fbc76dbfba7
User <user@MacBook-Pro-User.local>
2024-03-19 21:06:03 +0100

    c01ex03

---
+++ ex03/ft_div_mod.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+
+int	main(void)
+{
+	int a;
+	int b;
+	int div;
+	int mod;
+
+	a = 7;
+	b = 2;
+	
+	printf("a : %d, b : %d\n", a, b);
+	ft_div_mod(a, b, &div, &mod);
+	printf("a : %d, b : %d, div : %d, mod : %d\n", a, b, div, mod);
+	return (0);
+}
